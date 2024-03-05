40e42683c2b90c40a51c7413dc66c108f4a1d003
berunda <jonberunda@gmail.com>
2024-03-05 08:42:35 -0500

    forms

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-
+import { SessionProvider } from "next-auth/react";
 interface ProviderProps {
   children: React.ReactNode
 }
 export default function Providers({ children }: ProviderProps) {
   return (
-    <NextUIProvider>{children}</NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
   )
 }
